f47d9795f71dff320f3a1abfe8b571fdc955fadb
bobabeans <twilight_forest@icloud.com>
2025-10-05 04:17:36 -0400

    left/right player rotation

--- main.py
+++ main.py
@@ -17,7 +17,9 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
         pygame.Surface.fill(screen, "black")
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
