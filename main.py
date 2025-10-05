b9ec8a212372b10c14f439e8acdc07ae0054c94c
bobabeans <twilight_forest@icloud.com>
2025-10-05 17:21:37 -0400

    feat: introduce pygame groups and refactor game loop

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock  = pygame.time.Clock()
     dt = 0 # delta time
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(x, y)
 
     while True:
@@ -19,8 +24,9 @@ def main():
                 return
 
         pygame.Surface.fill(screen, "black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for objects in drawable:
+            objects.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60) / 1000
